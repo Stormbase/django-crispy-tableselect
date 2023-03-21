d4ee1e17786b9e45a41503de240a76b4ac0ce2bc
Martijn Jacobs <maerteijn@gmail.com>
2023-03-21 16:53:09 +0100

    Create the table helper manually: Also feed the choices from the table data

--- src/techonomy/django/django_tableselect/helpers.py
+++ src/techonomy/django/django_tableselect/helpers.py
@@ -1,29 +1,48 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.db.models.query import QuerySet
 from django.utils.translation import gettext_lazy as _
+from django_tables2 import Table
+
 from .columns import CheckBoxColumn
 
 
 class TableSelectHelper:
     """Helper that houses various features related to TableSelect."""
 
+    # Field of the record that holds the value to use in the form checkbox.
     value_field = "id"
-    """Field of the record that holds the value to use in the form checkbox."""
 
-    def __init__(self, column_name, table_class, table_data, table_kwargs):
+    # Field of the record that holds the name to use in the form checkbox.
+    value_name = "name"
+
+    def __init__(self, column_name, table_class, table_data, table_kwargs={}):
+        if not issubclass(table_class, Table):
+            msg = f"{repr(table_class)} must be a subclass of {repr(Table)}"
+            raise ImproperlyConfigured(msg)
+
         self.column_name = column_name
         self.table_class = table_class
         self.table_data = table_data
         self.table_kwargs = table_kwargs
 
+    @property
+    def choices(self):
+        if isinstance(self.table_data, QuerySet):
+            return self.table_data.values_list(self.value_field, self.value_name)
+
+        # Default, we assume a list of dictionaries here with
+        # 'value_field' and 'value_name' as keys
+        return [(x[self.value_field], x[self.value_name]) for x in self.table_data]
 
     def get_accessible_label(self, record):
         """Return the accessible label to associate with the form checkbox.
 
-        Defaults to `str(record)` for objects, or the value of a key `name` for dictionaries.
+        Defaults to `str(record)` for objects, or the value of a key self.value_name for dictionaries.
 
         This benefits users of assistive technology like screenreaders."""
 
         if isinstance(record, dict):
-            obj_name = record.get("name")
+            obj_name = record.get(self.value_name)
         else:
             obj_name = str(record)
 
