c2929051fd516fbd98b6f08072f456cd509a98b8
Storm Berê Heg <storm@stormheg.co>
2023-03-26 16:41:11 +0200

    Fix selected values

--- src/techonomy/django/django_tableselect/layout.py
+++ src/techonomy/django/django_tableselect/layout.py
@@ -15,11 +15,20 @@ class TableSelect(layout.TemplateNameMixin):
         self.name = name
         self.helper = helper
 
+
+    def format_value(self, value):
+        """Return selected values as a list."""
+        if value is None:
+            return []
+        if not isinstance(value, (tuple, list)):
+            value = [value]
+        return [str(v) if v is not None else "" for v in value]
+
     def render(self, form, context, template_pack=TEMPLATE_PACK, **kwargs):
         template = self.get_template_name(template_pack)
         bound_field = form[self.name]
 
-        selected_values = bound_field.value()
+        selected_values = self.format_value(bound_field.value())
 
         html_name = bound_field.html_name
         context.update(
